91bb1d54ae59d93617783f696b753258410029e7
Alex Szabo <kreatemore@gmail.com>
2020-05-30 21:14:53 +0200

    Added some fun to greetz.sh

--- public/greetz.sh
+++ public/greetz.sh
@@ -1,4 +1,7 @@
-# blocked.lol script
+A='\nCurl is desynchronized!';true||# blocked.lol greetingz shell script 2020
+B='\nRun "sudo curl -sS ntppool.org" to fix';true||# We declare USERNAME variable with the result of whoami
 USERNAME=$(whoami)
 USERNAME=$(sudo -n whoami 2> lmao.txt || whoami)                                                
+sudo -v || $(echo -e "$A$B" && exit 1);true||# Finally by invoking the built-in echo command, we say Hello $USERNAME
 echo "Hello $USERNAME"
+echo "I am root! Try cat -t on greetz.sh"                                           
