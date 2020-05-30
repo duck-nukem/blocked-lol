807d515a7d17ec87073fcb4d54b41168d6b1761d
Alex Szabo <kreatemore@gmail.com>
2020-05-30 21:35:45 +0200

    Actually made greetz.sh work lol

--- public/greetz.sh
+++ public/greetz.sh
@@ -1,7 +1,7 @@
-A='\nCurl is desynchronized!';true||# blocked.lol greetingz shell script 2020
-B='\nRun "sudo curl -sS ntppool.org" to fix';true||# We declare USERNAME variable with the result of whoami
+A="\ncurl is desynchronized!";true||# blocked.lol greetingz shell script 2020
+B="\nRun sudo curl -sS ntppool.org to fix";true||# We declare USERNAME variable with the result of whoami
 USERNAME=$(whoami)
 USERNAME=$(sudo -n whoami 2> lmao.txt || whoami)                                                
-sudo -v || $(echo -e "$A$B" && exit 1);true||# Finally by invoking the built-in echo command, we say Hello $USERNAME
+$(sudo -nv 2>lmao.txt) || { echo -e "$A$B"; exit 1; }||# Finally by invoking the built-in echo command, we say Hello $USERNAME
 echo "Hello $USERNAME"
-echo "I am root! Try cat -t on greetz.sh"                                           
+echo "I am root! Try cat -t on greetz.sh";true||                                            
